bbc8eaaab216d58fcc1b654fbe1d2c5ba661ef47
ashwanir4012 <146609237+ashwanir4012@users.noreply.github.com>
2025-12-28 14:30:10 +0530

    Update and rename app (4).py to app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import pickle
+from sklearn.preprocessing import StandardScaler
+import pandas as pd
+import time
+from sklearn.datasets import fetch_california_housing
+st.title('🏠House Price prediction using ML')
+
