2fd46f021024ccf7bae581d65a40bde9f7777c87
ashwanir4012 <146609237+ashwanir4012@users.noreply.github.com>
2025-12-28 16:36:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,9 +28,13 @@ scaled_X = scaler.fit_transform(X)
 
 final_value = scaler.transform([all_value])
 
+@st.cache_data
+def model_run(X,y):
+   model = RandomForestRegressor()
+   model.fit(X,y)
+   return model
 
-model = st.cache(RandomForestRegressor)()
-model.fit(X,y)
+model = model_run(X,y)
 house_price = model.predict(final_value)[0]
 
 with st.spinner('Predicting House Price'):
@@ -40,6 +44,7 @@ st.success(msg)
 
 st.markdown('''**Design And Developed By : Ashwani**''')
 # st.write(house_price)
+
 
 
 
