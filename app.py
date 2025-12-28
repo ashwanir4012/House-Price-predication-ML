fc0256e72b7d2c6bd935bc6838cd91fc7a960289
ashwanir4012 <146609237+ashwanir4012@users.noreply.github.com>
2025-12-28 16:20:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -35,11 +35,12 @@ house_price = model.predict(final_value)[0]
 
 with st.spinner('Predicting House Price'):
    time.sleep(1)
-msg = f'''House price is: ${house_price*100000}'''
+msg = f'''House price is: ${round(house_price*100000,2)}'''
 st.success(msg)
 
 st.markdown('''**Design And Developed By : Ashwani**''')
 # st.write(house_price)
+
 
 
 
