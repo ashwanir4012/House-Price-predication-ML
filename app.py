b123206e4310feb426e2973fecb6a090d0da2024
ashwanir4012 <146609237+ashwanir4012@users.noreply.github.com>
2025-12-28 16:00:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,38 +1,38 @@
 import streamlit as st
 import pickle
 from sklearn.preprocessing import StandardScaler
-from sklearn.ensemble import RandomForestRegressor
 import pandas as pd
 import time
 from sklearn.datasets import fetch_california_housing
-st.title('🏠House Price prediction using ML')
-st.image('https://clipart-library.com/images/8ixrEzkbT.gif')
+from sklearn.ensemble import RandomForestRegressor
+
+st.title(' 🏰 House Price prediction using ML')
+st.image('https://img.freepik.com/free-vector/charming-house-with-tree-illustration_1308-176337.jpg?semt=ais_hybrid&w=740&q=80')
 
 df = pd.read_csv('house_data.csv')
 X = df.iloc[:,:-3]
 y = df.iloc[:,-1]
 
-st.sidebar.title('🏠 Select House features')
-st.sidebar.image('https://clipart-library.com/images/8ixrEzkbT.gif')
+st.sidebar.title('🏡 Select House features')
+st.sidebar.image('https://img.freepik.com/free-vector/charming-house-with-tree-illustration_1308-176337.jpg?semt=ais_hybrid&w=740&q=80')
 all_value = []
-for i in X:
-  min_value = int(X[i].min())
-  max_value = int(X[i].max())
-  ans = st.sidebar.slider(f'Select {i} value',min_value, max_value)
-  all_value.append(ans)
+for i in X:  
+   min_value =  int(X[i].min())
+   max_value =  int(X[i].max())
+   ans = st.sidebar.slider(f'Select {i} value',min_value, max_value)
+   all_value.append(ans)
 
 # st.write(all_value)
-
 scaler = StandardScaler()
 scaled_X = scaler.fit_transform(X)
 
-final_value = scaler.tansform(all_vaule)
+final_value = scaler.transform([all_value])
+
 
 model = RandomForestRegressor()
 model.fit(X,y)
 house_price = model.predict(final_value)
 
-with st.spinner('Predicting House price'):
-  time.sleep(3)
+with st.spinner('Predicting House Price'):
+   time.sleep(3)
 st.write(house_price)
-
